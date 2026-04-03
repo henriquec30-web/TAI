a952113a57791bf9913d16e177590c9a21416c09
Henrique <henrique.c30@aluno.ifsc.edu.br>
2026-04-03 12:56:11 -0300

    Cria estrutura do roteiro 04 a partir do roteiro 03

---
+++ roteiro04/app.py
@@ -0,0 +1,25 @@
+from flask import Flask, jsonify, request
+from aluno import Aluno
+app = Flask(__name__)
+alunos = []
+aluno1 = Aluno("João", 17)
+aluno1.adicionar_nota(6)
+aluno1.adicionar_nota(7)
+aluno1.adicionar_nota(8)
+aluno2 = Aluno("Maria", 16)
+aluno2.adicionar_nota(5)
+aluno2.adicionar_nota(6)
+aluno2.adicionar_nota(7)
+alunos.append(aluno1)
+alunos.append(aluno2)
+@app.route("/alunos", methods=["GET"])
+def listar_alunos():
+    return jsonify([aluno.to_dict() for aluno in alunos])
+@app.route("/alunos", methods=["POST"])
+def adicionar_aluno():
+    dados = request.get_json()
+    novo_aluno = Aluno(dados["nome"], dados["idade"])
+    alunos.append(novo_aluno)
+    return jsonify(novo_aluno.to_dict()), 201
+if __name__ == "__main__":
+    app.run(debug=True) 
